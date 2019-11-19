9562249cef0c207da01e507d4d8cbeb75ff5dce2
ElderGnomeChild <daniellier12@gmail.com>
2019-11-19 13:52:11 -0700

    Started on server and client

---
+++ Connection.java
@@ -0,0 +1,77 @@
+/**
+ * This is the separate thread that services each
+ * incoming echo client request.
+ *
+ * @author Greg Gagne 
+ */
+
+/**
+Modified by Daniel Lier for HW5
+*/
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.nio.*;
+import java.net.InetAddress;
+
+import java.text.*;
+
+public class Connection implements Runnable
+{
+	public static final int BUFFER_SIZE = 1024;
+	private Socket	client;
+	public static String xml;
+
+	String line;
+	String resource;
+	
+	public Connection(Socket client) {
+		this.client = client;
+	}
+
+	public void process(Socket client) throws java.io.IOException {
+		BufferedReader fromClient = null;
+		OutputStream toClient = null;
+
+
+		try {
+			fromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			toClient = new BufferedOutputStream(client.getOutputStream());
+			
+			String getLine = fromClient.readLine();									//get the first line of the data sent from the client
+			
+
+            // byte[] contents = new byte[BUFFER_SIZE];							//send the file to the client
+            // int length;
+            // while ((length = fromFile.read(contents)) > 0) {
+            //     toClient.write(contents, 0, length);
+            //     toClient.flush();
+            // }
+
+		} catch (IOException ioe) {
+			System.err.println(ioe);
+		} finally {																	//close the streams
+			// if (fromClient != null) {
+			// 	fromClient.close();
+			// }
+			// if (toClient != null) {
+			// 	toClient.close();
+			// }
+		}
+
+	} 
+
+    /**
+     * This method runs in a separate thread.
+     */	
+	public void run() { 
+		try {
+			this.process(client);
+		}
+		catch (java.io.IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
+}
+
---
+++ EchoClient.java
@@ -0,0 +1,68 @@
+/**
+ * An echo client. The client enters data to the server, and the
+ * server echoes the data back to the client.
+ *
+ * @author - Greg Gagne
+ */
+
+
+/***
+ * Modified by Daniel Lier for Computer Networks HW2
+*/
+
+import java.net.*;
+import java.io.*;
+
+public class EchoClient
+{
+	public static final int DEFAULT_PORT = 1337;			// changed the port here
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length != 2) {
+			System.err.println("Usage: java EchoClient <echo server> <DNS lookup>");
+			System.exit(0);
+		}
+		
+		BufferedReader networkBin = null;	// the reader from the network
+		PrintWriter networkPout = null;		// the writer to the network
+		BufferedReader localBin = null;		// the reader from the local keyboard
+		Socket sock = null;			// the socket
+		String addr;
+		
+		try {
+			sock = new Socket(args[0], DEFAULT_PORT);
+			addr = args[1];								//set addr to second arg, this will be sent to the sever to be converted to an IP address
+			
+			// set up the necessary communication channels
+			networkBin = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			localBin = new BufferedReader(new InputStreamReader(System.in));
+			
+			/**
+			 * a PrintWriter allows us to use println() with ordinary
+			 * socket I/O. "true" indicates automatic flushing of the stream.
+			 * The stream is flushed with an invocation of println()
+			 */
+			networkPout = new PrintWriter(sock.getOutputStream(),true);
+			
+			/**
+			 * Read from the string and send it to the echo server.
+			 */
+				String line = addr;						
+				networkPout.println(line);					//send addr to client
+				System.out.println("Server: " + networkBin.readLine()); //print client response
+		}
+		catch (IOException ioe) {
+			System.err.println(ioe);
+		}
+		finally {
+			if (networkBin != null)
+				networkBin.close();
+			if (localBin != null)
+				localBin.close();
+			if (networkPout != null)
+				networkPout.close();
+			if (sock != null)
+				sock.close();
+		}
+	}
+}
